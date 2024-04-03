9d18c6fdf413d0b73051507a71bc5c279bd499da
cristic1988 <cristic8@gmail.com>
2024-04-03 21:47:26 +0300

    first commit

---
+++ src/DrinksMachine.java
@@ -0,0 +1,74 @@
+public enum DrinksMachine {
+    COFFEE, TEA, LEMONADE, MOCHITO, MINERAL_WATER, COCA_COLA;
+}
+
+class Drinks {
+    public static final double COFFEE_PRICE = 2.0;
+    public static final double TEA_PRICE = 1.5;
+    public static final double LEMONADE_PRICE = 2.5;
+    public static final double MOCHITO_PRICE = 3.0;
+    public static final double MINERAL_WATER_PRICE = 1.0;
+    public static final double COCA_COLA_PRICE = 2.0;
+    private static double totalAmount = 0.0;
+    private static int totalDrinks = 0;
+
+    public static void makeDrink(DrinksMachine drink) {
+        switch (drink) {
+            case COFFEE:
+                System.out.println("Please take coffee");
+                break;
+            case TEA:
+                System.out.println("Please take tea");
+                break;
+            case LEMONADE:
+                System.out.println("Please take lemonade");
+                break;
+            case MOCHITO:
+                System.out.println("Please take mochito");
+                break;
+            case MINERAL_WATER:
+                System.out.println("Please take mineral water");
+                break;
+            case COCA_COLA:
+                System.out.println("Please take Coca Cola");
+                break;
+            default:
+                System.out.println("Invalid drink choice.");
+
+        }
+
+        totalDrinks++;
+
+    }
+
+    public static void calculateTotalAmount(DrinksMachine drink) {
+        switch (drink) {
+            case COFFEE:
+                totalAmount += COFFEE_PRICE;
+                break;
+            case TEA:
+                totalAmount += TEA_PRICE;
+                break;
+            case LEMONADE:
+                totalAmount += LEMONADE_PRICE;
+                break;
+            case MOCHITO:
+                totalAmount += MOCHITO_PRICE;
+                break;
+            case MINERAL_WATER:
+                totalAmount += MINERAL_WATER_PRICE;
+                break;
+            case COCA_COLA:
+                totalAmount += COCA_COLA_PRICE;
+                break;
+
+        }
+
+    }
+
+    public static void displayTotal() {
+        System.out.println("Total drinks: " + totalDrinks);
+        System.out.println("Total amount: " + totalAmount);
+
+    }
+}
---
+++ src/VendingMachineWithDrinks.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+public class VendingMachineWithDrinks {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+
+            System.out.println("Select a drink:");
+            System.out.println("1. Coffee - price " + Drinks.COFFEE_PRICE);
+            System.out.println("2. Tea - price " + Drinks.TEA_PRICE);
+            System.out.println("3. Lemonade - price " + Drinks.LEMONADE_PRICE);
+            System.out.println("4. Mochito - price " + Drinks.MOCHITO_PRICE);
+            System.out.println("5. Mineral water - price " + Drinks.MINERAL_WATER_PRICE);
+            System.out.println("6. Coca Cola - price " + Drinks.COCA_COLA_PRICE);
+            System.out.println("7. Exit");
+
+            int choice = scanner.nextInt();
+            DrinksMachine drink;
+
+            switch (choice) {
+                case 1:
+                    drink = DrinksMachine.COFFEE;
+                    break;
+                case 2:
+                    drink = DrinksMachine.TEA;
+                    break;
+                case 3:
+                    drink = DrinksMachine.LEMONADE;
+                    break;
+                case 4:
+                    drink = DrinksMachine.MOCHITO;
+                    break;
+                case 5:
+                    drink = DrinksMachine.MINERAL_WATER;
+                    break;
+                case 6:
+                    drink = DrinksMachine.COCA_COLA;
+                    break;
+                case 7:
+                    Drinks.displayTotal();
+                    return;
+                default:
+
+                    System.out.println("Invalid choice.");
+                    continue;
+
+            }
+
+            Drinks.makeDrink(drink);
+            Drinks.calculateTotalAmount(drink);
+
+        }
+    }
+}
